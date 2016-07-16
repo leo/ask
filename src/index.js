2ab8d9b81c27bfee7da61eec64c13a69479eae78
Leo Lamprecht <mindrun@icloud.com>
2016-07-16 14:22:53 +0200

    Basic mechanic

--- src/index.js
+++ src/index.js
@@ -1,4 +1,37 @@
-export function prompt (questions) {
-  console.log(questions)
-  return 'test'
+export async function prompt (questions) {
+  let list = [],
+      details = {}
+
+  for (let question of questions) {
+    let asker = new Promise((resolve, reject) => {
+      resolve('test')
+    })
+
+    let index = list.push(asker)
+    details[index] = question
+  }
+
+  return new Promise((resolve, reject) => {
+    const asked = Promise.all(list)
+
+    asked.then(answers => {
+      let results = {}
+
+      for (let answer in answers) {
+        let result = answers[answer],
+            info = details[parseInt(answer) + 1]
+
+        if (!info.name) {
+          reject('No name defined for question')
+          break
+        }
+
+        results[info.name] = result
+      }
+
+      resolve(results)
+    })
+
+    asked.catch(reject)
+  })
 }
